fc5f04418a6c9957b9e4b423b872eb01cf0f386a
vothaihoa1998 <thaihoavo1998@gmail.com>
2020-10-23 09:58:27 +0700

    feat(heade-footer):update responsive footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,16 @@ $('.dropdown').on('click', function(e) {
     var dropdown = target.closest('.dropdown');
     return !dropdown.hasClass('open') || !target.hasClass('dropdown-menu__search__input');
 });
+
+(function($) { // Begin jQuery
+    $(function() { // DOM ready
+        // Toggle open and close nav styles on click
+        $('#nav-toggle').click(function() {
+            $('nav ul').slideToggle();
+        });
+        // Hamburger to X toggle
+        $('#nav-toggle').on('click', function() {
+            this.classList.toggle('active');
+        });
+    }); // end DOM ready
+})(jQuery); // end jQuery
